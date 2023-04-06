5d04e9d20af41c215a44f0ae87d6e38ba902f7e0
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-04-06 16:07:09 +0530

    ADD : Hamburger Menu

--- src/style-guide/components/navbar/index.jsx
+++ src/style-guide/components/navbar/index.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import styles from "./style.module.scss";
 import { signInWithGoogle } from "@/utils/firebase";
 
-const Navbar = () => {
+const Navbar = ({ mobileHamClicked }) => {
   return (
-    <div className={styles.navbar}>
-      <div className={styles.logo}>BIDBAZZAR</div>
-      <div className={styles.nav_links}>
-        <a href="/">HOME</a>
-        <a href="/profile">PROFILE</a>
-        <a href="/market">MARKET</a>
-      </div>
-      <div className={styles.callToAction}>
-        <div className={styles.btn} onClick={signInWithGoogle}>
-          SignIn
+    <div className={styles.navWrapper}>
+      <div
+        className={`${styles.navbar} ${
+          mobileHamClicked ? styles.navShow : styles.navHide
+        }`}
+      >
+        <div className={styles.logo}>BIDBAZZAR</div>
+        <div className={styles.nav_links}>
+          <a href="/">HOME</a>
+          <a href="/profile">PROFILE</a>
+          <a href="/market">MARKET</a>
+        </div>
+        <div className={styles.callToAction}>
+          <div className={styles.btn} onClick={signInWithGoogle}>
+            SignIn
+          </div>
         </div>
       </div>
     </div>
