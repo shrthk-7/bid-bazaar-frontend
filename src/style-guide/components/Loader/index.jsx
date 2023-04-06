0974075c043f0b4f6ade4355fac6e43cf1667505
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-04-06 22:35:01 +0530

    UPDATE : rings

--- src/style-guide/components/Loader/index.jsx
+++ src/style-guide/components/Loader/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./style.module.scss";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className={styles.loader}>
+      Loader
+    </div>
+  )
 };
 
 export default Loader;
