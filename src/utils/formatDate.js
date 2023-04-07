1c41b1db59e8659dd38a566d5ca6ee35bc3f3dab
Sharthak Sharma <sharthaksharma.ss@gmail.com>
2023-04-08 02:00:32 +0530

    integration in product page

---
+++ src/utils/formatDate.js
@@ -0,0 +1,11 @@
+export const formatDate = (seconds) => {
+  seconds = Number(seconds / 1000);
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+
+  const dDisplay = d > 0 ? d + "d " : "";
+  const hDisplay = h > 0 ? h + "h " : "";
+  const mDisplay = m > 0 ? m + "m " : "";
+  return dDisplay + hDisplay + mDisplay;
+};
