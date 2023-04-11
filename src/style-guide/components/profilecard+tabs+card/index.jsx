83a5a03551920b83af5b1587d9da762e982c66c5
Sharthak Sharma <sharthaksharma.ss@gmail.com>
2023-04-11 15:46:29 +0530

    predeploy

--- src/style-guide/components/profilecard+tabs+card/index.jsx
+++ src/style-guide/components/profilecard+tabs+card/index.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Profilecard from "../profilecard";
 import Tabs from "../tabs";
 import style from "./style.module.scss";
+import Spinner from "../spinner";
 
 export default function Profile() {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: localStorage.getItem("token"),
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.status === "success") {
+        setUser(data.user);
+      }
+    }
+    fetchData();
+    setLoading(false);
+  }, []);
+
   return (
     <div className={style.page}>
-      <Profilecard />
-      <Tabs />
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Profilecard slides={user} />
+          <Tabs slides={user} />
+        </>
+      )}
     </div>
   );
 }
